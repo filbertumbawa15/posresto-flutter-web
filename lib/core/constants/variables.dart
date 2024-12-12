572e60b3d301944512ed982cb139e6ac60329aaa
denicetas15 <filbertumbawa@gmail.com>
2024-12-12 23:19:33 +0700

    flutter web init

---
+++ lib/core/constants/variables.dart
@@ -0,0 +1,8 @@
+class Variables {
+  static const String appName = 'Resto App';
+  static const String apiVersion = 'v1';
+  static const String baseUrl =
+      'http://47.129.227.113:8880/laravel-posresto-backend/public';
+  // http://47.129.227.113:8880/laravel-posresto-backend/public
+  // static const String baseUrl = 'https://resto.jagofullstack.com';
+}
